9403e9efae2cd5e30dfde8dd0572e4ea57cdc205
Mehedi Hasan <mhedih32@gmail.com>
2024-03-19 07:39:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-blue-600 text-center items-center py-5 border-2 border-red-700 font-extrabold bg-gray-400'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
